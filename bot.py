4f65de3992c0dcf5cc28ffa6af56861e16ff1e1f
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2022-07-13 22:30:58 +0300

    don't greet itself

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
-import sys
+import os
 import telebot
 
-bot = telebot.TeleBot(sys.argv[1])
+bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
 
 
 @bot.message_handler(content_types='new_chat_members')
 def handle_message(message):
-    bot.send_animation(message.chat.id, open("welcome.gif", 'rb'), reply_to_message_id=message.id)
+    if message.json['new_chat_member']['id'] != bot.get_me().id:
+        bot.send_animation(message.chat.id, open("welcome.gif", 'rb'), reply_to_message_id=message.id)
 
 
 bot.polling(none_stop=True)
